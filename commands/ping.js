fb532a32b6a6e51f66b3c0e46f121e4497dedf0b
Limon-bot-pixel <ls0546744@gmail.com>
2025-11-20 07:57:39 +0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,22 @@
+// ping.js
+// Limon Bot - Ping Command
 
+module.exports = async (sock, m) => {
+    try {
+        const start = Date.now();
+
+        const msg = await sock.sendMessage(m.key.remoteJid, { text: "🏓 Pinging..." });
+
+        const end = Date.now();
+        const pingSpeed = end - start;
+
+        await sock.sendMessage(
+            m.key.remoteJid,
+            { text: `✅ *Pong!* \n⚡ Speed: *${pingSpeed}ms*` },
+            { quoted: msg }
+        );
+
+    } catch (error) {
+        console.log("Ping Command Error:", error);
+    }
+};
